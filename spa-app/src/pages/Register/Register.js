9f85f13ec8b05d15d913e5061648b8b29684a856
Dainius Skiparis <109749812+DainiusSkiparis@users.noreply.github.com>
2023-11-09 01:08:35 +0200

    Revert "Develop"

--- spa-app/src/pages/Register/Register.js
+++ spa-app/src/pages/Register/Register.js
@@ -1,96 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "../../components/Button/Button";
+import RegisterForm from "../../components/Form/RegisterForm";
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const userData = {
-      firstname: firstName,
-      lastname: lastName,
-      email: email,
-      password: password,
-    };
-
-    try {
-      const response = await fetch("http://localhost:5000/admins/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
-
-      console.log(userData);
-
-      if (response.ok) {
-        console.log("Registracija pavyko!");
-        navigate("/singin");
-      } else {
-        console.error("Registracija nepavyko");
-      }
-    } catch (error) {
-      console.error("Klaida atliekant registraciją:", error);
-    }
+  const onRegistrationSuccess = () => {
+    alert("Sign-up was successful!");
+    navigate("/singin");
   };
 
   return (
     <div>
-      <h2>Registracija</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">El. paštas (prisijungimas vyks su juo)</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Jūsų el. paštas"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="firstName">Vardas</label>
-          <input
-            type="text"
-            id="firstName"
-            placeholder="Jūsų vardas"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName">Pavardė</label>
-          <input
-            type="text"
-            id="lastName"
-            placeholder="Jūsų pavardė"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Slaptažodis</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Jūsų slaptažodis"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <Button type="submit" text="Registruotis" design="green"></Button>
-      </form>
+      <h2>Registration</h2>
+      <RegisterForm onSuccessfulRegister={onRegistrationSuccess} />
     </div>
   );
 };

449eb22879e0d0af764cd35dc1692793c2da28c9
Dainius Skiparis <109749812+DainiusSkiparis@users.noreply.github.com>
2023-11-09 01:09:14 +0200

    Revert "Develop"

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -14,38 +14,18 @@ router.get("/", auth, async (_, res) => {
 });
 
 // POST - Sukurti vartotoją
-router.post("/register", auth, async (req, res) => {
+router.post("/create", async (req, res) => {
   try {
-    const { firstname, lastname, email, visit } = req.body;
+    const { name, email, password } = req.body;
     const newUser = new User({
-      firstname,
-      lastname,
+      name,
       email,
-      visit,
+      password,
     });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
     res.status(400).json({ error: error.message });
-  }
-});
-
-// DELETE užklausos apdorojimas
-router.delete("/:id", auth, async (req, res) => {
-  console.log(userId);
-  try {
-    const userId = req.params.id;
-    const user = await User.findByIdAndRemove(userId);
-    console.log(userId);
-
-    if (!user) {
-      return res.status(404).json({ error: "Vartotojas nerastas" });
-    }
-
-    res.json({ message: "Vartotojas ištrintas sėkmingai" });
-  } catch (error) {
-    console.error("Ištrynimo klaida:", error);
-    res.status(500).json({ error: "Ištrinant vartotoją įvyko klaida" });
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const connectDb = require("./config/database.js");
 const usersController = require("./controllers/usersController.js");
 const adminController = require("./controllers/adminController.js");
@@ -9,11 +8,10 @@ const DB = require("./common/constants/dataBase.js");
 connectDb();
 const server = express();
 
-server.use(cors());
 server.use(express.json());
 
-server.use("/users", usersController);
-server.use("/admins", adminController);
+server.use("/spaapp/users", usersController);
+server.use("/spaapp/admins", adminController);
 
 server.listen(DB.PORT, () => {
   console.log(`Server is running on port ${DB.PORT}`);
